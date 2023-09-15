94576d74adcce938de5f82048ba53d3fc1168acc
amirmuhammedtk <138512502+amirmuhammedtk@users.noreply.github.com>
2023-09-15 19:48:19 +0530

    second commit

--- src/Project.jsx
+++ src/Project.jsx
@@ -4,7 +4,43 @@ function Project() {
   return (
     <div>
       <form>
-        
+      <div className="row firr">
+          {/* first col */}
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+            <div className='out'>
+            <h1 className='edu'>Projects</h1>
+            <div className='box avat '>
+              <div className='ins'>
+              <h3></h3>
+              <h5 className='uni'></h5>
+              <h6></h6></div>
+            </div>
+            </div>
+            {/* next row */}
+            <div className='out'>
+            {/* <h1 className='edu'>Education</h1> */}
+            <div className='box avat '>
+              <div className='ins'>
+              <h3></h3>
+              <h5 className='uni'></h5>
+              <h6></h6></div>
+            </div>
+            </div>
+          </div>
+          {/* second col */}
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6">
+          <div className='out'>
+
+       
+            <div className='box avat '>
+              <div className='ins'>
+              <h3></h3>
+              <h5 className='uni'></h5>
+              <h6></h6></div>
+            </div>
+            </div>
+          </div>
+        </div>
       </form>
     </div>
   )
